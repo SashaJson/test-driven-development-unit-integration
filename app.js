4b459588e4c9c446952fc1c8b7844114e8c80443
SashaJson <sashamiller666@gmail.com>
2021-04-18 23:17:44 +0300

    added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json('Hello world');
+});
+
+app.listen(3000, () => {
+    console.log('Server is now running!')
+});
