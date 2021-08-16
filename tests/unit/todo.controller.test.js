a1d2aae549e041a23c356926434d4f0a0a1890f2
SashaJson <sashamiller666@gmail.com>
2021-08-16 16:04:40 +0300

    mock for TodoModel method "create"

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const TodoController = require('../../controllers/todo.controller');
+const TodoModel = require('../../model/todo.mode');
+
+TodoModel.create = jest.fn();
 
 describe('TodoController.createTodo', () => {
 
     it('should have a createTodo function', async () => {
+
        expect(typeof TodoController.createTodo).toBe('function');
+
+    });
+
+    it('should call TodoModel.create', async () => {
+
+        TodoController.createTodo();
+        expect(TodoModel.create()).toBeCalled();
+
     });
 
 }); // describe (TodoController.createTodo)
