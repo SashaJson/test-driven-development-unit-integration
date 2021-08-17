a51094d45f0c6f00a40262b0f74b09927cd59574
SashaJson <sashamiller666@gmail.com>
2021-08-17 19:58:08 +0300

    small fix in controller and app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const todoRoutes = require('./routes/todo.routes');
 const app = express();
 
+app.use(express.json());
+
 app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
