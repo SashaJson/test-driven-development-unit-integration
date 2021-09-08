d45201d4a8623a94e3762d446f4d991e80bc8b9f
SashaJson <sashamiller666@gmail.com>
2021-09-09 01:29:51 +0300

    added handle error testing in getTodos

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -37,6 +37,14 @@ describe('TodoController.getTodos', () => {
         expect(res._getJSONData()).toStrictEqual(allTodos);
     });
 
+    it('should handle errors in getTodos', async () => {
+        const errorMessage = { message: 'Error finding' };
+        const rejectPromise = Promise.reject(errorMessage);
+        TodoModel.find.mockReturnValue(rejectPromise);
+        await TodoController.getTodos(req, res, next);
+        expect(next).toHaveBeenCalledWith(errorMessage);
+    });
+
 });
 
 describe('TodoController.createTodo', () => {
