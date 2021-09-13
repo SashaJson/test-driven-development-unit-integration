8c19e003517b9846d6e7c6fa23d9bc6def6d1f20
SashaJson <sashamiller666@gmail.com>
2021-09-13 12:59:43 +0300

    added unit test "updateTodo" return json data and status 200

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -43,6 +43,17 @@ describe('TodoController.updateTodo', () => {
 
     });
 
+    it('should return a response with json data and http code 200', async () => {
+
+        TodoModel.findByIdAndUpdate.mockReturnValue(newTodo);
+
+        await TodoController.updateTodo(req, res, next);
+        expect(res._isEndCalled()).toBeTruthy();
+        expect(res.statusCode).toBe(200);
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+
+    });
+
 }); // TodoController.updateTodo
 
 describe('TodoController.getTodoById', () => {
